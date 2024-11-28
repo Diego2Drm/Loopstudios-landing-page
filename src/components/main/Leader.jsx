03e4fad1696621e3943e2b491929c508115e1c4d
Diego Ramirez <diego.ramirez2d03@gmail.com>
2024-11-27 21:39:49 -0600

    Media Query Leader

--- src/components/main/Leader.jsx
+++ src/components/main/Leader.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import '../../scss/components/Leader.scss'
-import imageInteractive from '../../assets/mobile/image-interactive.jpg'
 
 function Leader() {
   return (
     <section className="content__leader">
-      <img src={imageInteractive} alt="image-interactive" />
-      <h2>The Leader in interactive vr</h2>
-      <p>Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of the best companies around the globe. Our award-winning creations have transformed businesses through digital experiences that bind to their brand.</p>
+      <div className="content__leader--img"></div>
+      <div className="content__leader-info">
+        <h2>The Leader in interactive vr</h2>
+        <p>
+          Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of the best companies around the globe. Our award-winning creations have transformed businesses through digital experiences that bind to their brand.
+        </p>
+      </div>
     </section>
   );
 };
