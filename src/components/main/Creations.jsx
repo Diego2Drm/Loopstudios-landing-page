5429e5594574bdee4a354972585b858e37a86e70
Diego Ramirez <diego.ramirez2d03@gmail.com>
2024-11-28 21:01:52 -0600

    Media Query Creations

--- src/components/main/Creations.jsx
+++ src/components/main/Creations.jsx
@@ -1,21 +1,29 @@
 import { infoCreations } from "../../utils/CreationsInfo";
 import '../../scss/components/Creations.scss'
+import { Link } from "./Link";
 
 function Creations() {
 
   return (
     <section className="content__creations">
-      <h2>Our Creations</h2>
-      {
-        infoCreations.map((item, i) => (
-          <div className="content__creations-info" key={i}>
-            <img src={item.imgMobile} alt={item.title} />
-            <p>{item.title}</p>
-          </div>
-        ))
-      }
+      <div className="content__creations-title">
+        <h2>Our Creations</h2>
+        <Link />
+
+      </div>
+      <div className="content__creations-grid">
+        {
+          infoCreations.map((item, i) => (
+            <figure className="content__creations-info" key={i}>
+              <img className="img--mobile" src={item.imgMobile} alt={item.title} />
+              <img className="img--desktop" src={item.imgDesktop} alt={item.title} />
+              <p>{item.title}</p>
+            </figure>
+          ))
+        }
+      </div>
       <div className="content__creations-link">
-        <a href="#">See all</a>
+        <Link />
       </div>
     </section>
   )
